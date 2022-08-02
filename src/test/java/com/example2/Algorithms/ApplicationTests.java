a39f1e49f0dbc708fce4b0bad57ba8a0d6008a7f
Elena <biertli@bk.ru>
2022-08-02 21:15:29 +0300

    Initial commit

---
+++ src/test/java/com/example2/Algorithms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example2.Algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
